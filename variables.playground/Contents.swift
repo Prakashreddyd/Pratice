50dd8b4d24d53709c745d88baeb8a6cfa28812ba
Prakashreddy <prakashreddydubbaka@Prakashreddys-MacBook-Pro.local>
2016-09-19 17:14:47 +0530

    first

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var firstvariable = 15
+var secondVariable = 5
+var thirdvariable = firstvariable + secondVariable
+var fourthvariable = firstvariable * secondVariable
+var fifthvariable = firstvariable - secondVariable
+var sixthvariable = firstvariable / secondVariable
+
+
